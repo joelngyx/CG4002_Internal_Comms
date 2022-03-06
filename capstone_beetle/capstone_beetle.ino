727642bb4fc7d529caba0235eb08c17662e5fd60
joelngyx <joelngyx@gmail.com>
2022-03-07 03:36:59 +0800

    - Stop-N-Wait is approx 1 Hz instead of 10 Hz - Implemented a rudimentary sliding window protocol, able to achieve about 26 Hz - Changed packet sizes to 20 bytes for simplicity sake - Implemented and tested multi-threading environment - Implemented a more straightforward way to read packets

--- capstone_beetle/capstone_beetle.ino
+++ capstone_beetle/capstone_beetle.ino
@@ -1,32 +1,35 @@
 bool completed_handshake;
+uint8_t sequence_counter;
 
-void setup() {
-  Serial.begin(115200); 
-  // B4 52 A9 09 B6 D2
-  completed_handshake = false;
-  pinMode(13, OUTPUT);
-}
 
 struct DataPacket {
-  uint8_t beetle_id;
+  uint8_t beetle_id; 
   uint8_t packet_id;
-  int payload_1;
-  int payload_2;
-  int payload_3;
-  uint8_t crc;
+  uint8_t sequence_id;
+  uint16_t payload_1;
+  uint16_t payload_2;
+  uint16_t payload_3;
+  uint16_t payload_4;
+  uint16_t payload_5;
+  uint16_t payload_6;
+  uint16_t buffer_1;
+  uint16_t buffer_2;
+  uint8_t buffer_3;
 };
 
 void send_packet(
   uint8_t beetle_id,
   uint8_t packet_id,
-  int payload_1,
-  int payload_2,
-  int payload_3,
-  uint8_t crc) {
-  // Dummy data
+  uint8_t sequence_id,
+  uint16_t payload_1,
+  uint16_t payload_2,
+  uint16_t payload_3,
+  uint16_t payload_4,
+  uint16_t payload_5,
+  uint16_t payload_6) {
   DataPacket packet;
-  packet.beetle_id = beetle_id;
-  packet.packet_id = packet_id;
+  packet.beetle_id = (uint8_t)beetle_id;
+  packet.packet_id = (uint8_t)packet_id;
   /*
    * packet id and their purposes
    * 1: SYN
@@ -40,28 +43,57 @@ void send_packet(
    * 8: START
    * 10: FIN
    */
-  packet.payload_1 = payload_1;
-  packet.payload_2 = payload_2;
-  packet.payload_3 = payload_3;
-  packet.crc = crc;
+  packet.sequence_id = (uint8_t)sequence_id;
+  packet.payload_1 = (uint16_t)payload_1;
+  packet.payload_2 = (uint16_t)payload_2;
+  packet.payload_3 = (uint16_t)payload_3;
+  packet.payload_4 = (uint16_t)payload_4;
+  packet.payload_5 = (uint16_t)payload_5;
+  packet.payload_6 = (uint16_t)payload_6;
+  packet.buffer_1 = (uint16_t)0;
+  packet.buffer_2 = (uint16_t)0;
+  packet.buffer_3 = (uint8_t)0;
 
+  
   Serial.write((uint8_t *)&packet, sizeof(packet));
 }
 
 
+void setup() {
+  Serial.begin(115200); 
+  // B4 52 A9 09 B6 D2
+  completed_handshake = false;
+  sequence_counter = 0;
+  pinMode(13, OUTPUT);
+}
+
+
+
 void loop() {
   if(Serial.available()){
+    if(sequence_counter == 49) {
+      sequence_counter = 50;
+    }
     // UNO receives data in ASCII 
     // e.g. "A" has value 65
     // Serial.read clears the buffer
     switch(Serial.read()) {
       case 65: 
         // start handshake
-        send_packet(1,2,0,0,0,0);
+        send_packet(1,2,0,0,0,0,0,0,0);
+        break;
       case 66:
         // complete handshake
         completed_handshake = true;
         digitalWrite(13, HIGH);
+        break;
+      case 67:
+      case 68:
+        int count = 0;
+        for(count = 0; count < 50; count++){
+          send_packet(1,5,count,4,5,6,7,8,9); 
+        }
+        break;
       default:
         break;
     }
--- central_device.py
+++ central_device.py
@@ -1,7 +1,8 @@
+from ctypes import sizeof
 from logging.handlers import BaseRotatingHandler
 from signal import signal
 from bluepy import btle
-import signal
+import threading
 import time
 
 """
@@ -23,56 +24,117 @@ bluetoothctl for various bluetooth functions
 packet ids
 - A: SYN (handshake)
 - B: ACK (handshake)
-- C: ACK (for receiving data)
+- C: REQ (request for data)
+- D: ACK (acknowledge data)
 """
 
 mac_address_1 = "80:30:DC:D9:1F:A9"
+mac_address_2 = "30:E2:83:AE:68:71"
 service = "0000dfb0-0000-1000-8000-00805f9b34fb"
 beetle_characteristic = "0000dfb1-0000-1000-8000-00805f9b34fb"
-buffer = []
+# this global variable stores incoming data
+# 1 list for each beetle
+buffer = [[],[]]
+
+
+def add_two_binaries(val1, val2):
+    temp_binary = 0b0000_0000_0000_0000
+    temp_binary = temp_binary|val1
+    temp_binary = temp_binary << 8
+    temp_binary = temp_binary | val2
+    return temp_binary
+
+
+
+class Packet:
+    def __init__(
+        self, 
+        beetle_id, 
+        packet_id,
+        payload_1,
+        payload_2,
+        payload_3,
+        payload_4,
+        payload_5,
+        payload_6):
+        self.beetle_id = beetle_id,
+        self.packet_id = packet_id,
+        self.payload_1 = payload_1
+        self.payload_2 = payload_2
+        self.payload_3 = payload_3 
+        self.payload_4 = payload_4
+        self.payload_5 = payload_5
+        self.payload_6 = payload_6 
 
 
 class Beetle_Delegate(btle.DefaultDelegate):
-    def init(self):
-        btle.DefaultDelegate.init(self)
+    """
+    Enables the script to receive Bluetooth messages asynchronously,
+    processes incoming data
+    """
+    def __init__(self, params):
+        btle.DefaultDelegate.__init__(self)
+        self.beetle_id = params
+
 
     def handleNotification(self, cHandle, data):
-        # print("recieved, ", data)
-        decoded_data = data.decode()
-        """
-        Expects packets of the len 9
-        i.e. 
-        id|id|p_1a|p_1b|p_2a|p_2b|p
-        """
-        print(len(data))
         print("received, ", data)
-        print(" ", decoded_data)
-        buffer.append(data)
 
+        # each packet comes in the format
+        # [beetle_id][packet_id][sequence_id] //0,1,2
+        # [payload_1][payload_2][payload_3] //3,4,5,6,7,8
+        # [payload_4][payload_5][payload_6] //9,10,11,12,13,14
+        # remaining bytes are buffers
+
+        # Arrange packets
+        temp_packet = Packet(
+            data[0],
+            data[1],
+            add_two_binaries(data[3], data[4]),
+            add_two_binaries(data[5], data[6]),
+            add_two_binaries(data[7], data[8]),
+            add_two_binaries(data[9], data[10]),
+            add_two_binaries(data[11], data[12]),
+            add_two_binaries(data[13], data[14])
+        )   
+
+        buffer[self.beetle_id].append(temp_packet)
+        print(buffer[self.beetle_id][0].beetle_id)
+        print(buffer[self.beetle_id][0].packet_id)
+        print(buffer[self.beetle_id][0].payload_1)
+        print(buffer[self.beetle_id][0].payload_2)
+        print(buffer[self.beetle_id][0].payload_3)
+        print(buffer[self.beetle_id][0].payload_4)
+        print(buffer[self.beetle_id][0].payload_5)
+        print(buffer[self.beetle_id][0].payload_6)
 
 
 
 class Beetle_Connection:
-    def init(self, address):
+    def __init__(self, address, beetle_id):
         self.address = address
+        self.beetle_id = beetle_id
         self._periph = None
         self._periph_delegate = None
 
     
     def make_connection(self):
-        # establishes a connection to the specified beetle
+        """
+        Establishes a connection to the specified beetle
+        (id as provided by the instance of Beetle_Connection)
+        """
         self._periph = btle.Peripheral(self.address)
         print(self._periph.getState())
         print("connected!")
-        self._periph_delegate = Beetle_Delegate()
+        self._periph_delegate = Beetle_Delegate(self.beetle_id)
         self._periph.setDelegate(self._periph_delegate)
 
-        # svc = self._periph.getServiceByUUID("dfb0")
-        # ch = svc.getCharacteristics("dfb1")[0]
-        # # return ch
-    
     
     def reconnect(self):
+        """
+        Upon abrupt disconnection, the script will attempt to 
+        reconnect to the Beetle
+        """
         try:
             print("reconnecting")
             self.make_connection()
@@ -82,30 +144,144 @@ class Beetle_Connection:
 
     
     def disconnect(self):
-        # disconnects the beetle
+        """
+        Disconnects the beetle
+        """
         self._periph.disconnect()
         print("disconnected!")
 
     
     def write(self, val):
+        """
+        Writes the provided value val to the Beetle
+            - encoded by UTF-8
+            - the value will be received as its ASCII value
+            - e.g. A is 65
+        """
         for characteristic in self._periph.getCharacteristics():
             if characteristic.uuid == beetle_characteristic:
-                print("sending 'A' packet to %s" % (self._periph.addr))
+                print("sending", val, "packet to ", self._periph.addr)
                 characteristic.write(bytes(val, "UTF-8"), withResponse=False)
 
     
     def do_handshake(self):
+        """
+        Conducts a 3-way handshake, i.e.
+            - Laptop -SYN-> Beetle
+            - Beetle -SY-ACK-> Laptop
+            - Laptop -ACK-> Laptop
+        Uses the values in buffer as a flag
+        """
         self.write("A")
         self._periph.waitForNotifications(2.0)
-        if buffer[0] == b'\x01\x02\x00\x00\x00\x00\x00\x00\x00':
-            buffer.clear()
+        if buffer[self.beetle_id][0].packet_id == (2,):
             print(buffer)
             self.write("B")
-        print("handshake completed!")
+            print("handshake completed!")
+        else:
+            print("handshake incomplete!")
+            # Connection may have been established, but the received
+            # SYN=ACK packet might have not been accepted
+            try:
+                self._periph.disconnect()
+            except Exception as e: 
+                print(e)
+            
+            self._periph = None
+        
+        buffer[self.beetle_id].clear()
+
+    
+    def stop_and_wait(self):
+        """
+        This function conducts a stop-and-wait protocol
+            - uses the buffer list to check if packets received are in
+              order
+            - self._periph.waitForNotifications enables the
+              asynchronous reception of packets
+            - when a packet is received, Beetle_Delegate's method(s)
+              are called. 
+              > if the packet is received successfully, the packet is
+                written onto the buffer list. The script acknowledges 
+                the received packet by sending a "D" packet to the 
+                Beetle
+              > if the packet is not received successfully, the packet
+                is not written onto the buffer list. The script will then
+                request for the packet to be re-sent
+        """
+        packet_count = 0
+        try:
+            for i in range(160):
+                # Tests Stop-n-Wait, for approx. 1 second
+                print("iteration, ", i)
+                if self._periph.waitForNotifications(0.001):
+                    if buffer[self.beetle_id][packet_count] is None:
+                        print("no input")
+                        self.write("C")
+                    else:
+                        self.write("D")
+                        packet_count += 1
+                        print("packet count, ", packet_count)
+                        print(buffer)
+                        print("success!")
+                    # self.write("C")
+                    continue
+            print("packets received = ", packet_count)
+            print(buffer)
+            self.disconnect()
+            return
+        except KeyboardInterrupt:
+            try:
+                self.disconnect()
+            except:
+                print("end of run")
+                return
+        except btle.BTLEException as e:
+            print("error, ", e)
+            self._periph = None
+            while self._periph is None:
+                self.reconnect()
+        except Exception as f:
+            if(self._periph is not None):
+                self.disconnect()
+            print("error, ", f)
+            self._periph = None
+            while self._periph is None:
+                self.reconnect()
+
+
+    def sliding_window(self):
+        try:
+            for i in range(120):
+                print("iteration, ", i)
+                if self._periph.waitForNotifications(0.01):
+                    continue
+        except KeyboardInterrupt:
+            try:
+                self.disconnect()
+            except:
+                print("end of run")
+                return
+        except btle.BTLEException as e:
+            print("error, ", e)
+            self._periph = None
+            while self._periph is None:
+                self.reconnect()
+        except Exception as f:
+            if(self._periph is not None):
+                self.disconnect()
+            print("error, ", f)
+            self._periph = None
+            while self._periph is None:
+                self.reconnect()
+
+        print(len(buffer[self.beetle_id]))
+        print(buffer)
+
+    
 
 
     def main_routine(self):
-        self._periph = None
         while self._periph is None:
             try:
                 self.make_connection()
@@ -114,45 +290,28 @@ class Beetle_Connection:
                 print("error, ", e)
                 time.sleep(2.0)
 
+        # To kick off the sending of data
+        # self.write("C")
 
+        # start = time.time()
+        # print("start", start)
+        # self.sliding_window()
+        # end = time.time()
+        # print("end", end)
+        # print("time elapsed in seconds", (end - start))
         while True:
-            try:
-                for i in range(100):
-                    if self._periph.waitForNotifications(0.01):
-                        continue
-                    print("waiting...")
-                self.disconnect()
-                return
-            except KeyboardInterrupt:
-                try:
-                    self.disconnect()
-                except:
-                    print("end of run")
-                    return
-            except btle.BTLEException as e:
-                print("error, ", e)
-                self._periph = None
-                while self._periph is None:
-                    self.reconnect()
-            except Exception as f:
-                if(self._periph is not None):
-                    self.disconnect()
-                print("error, ", f)
-                self._periph = None
-                while self._periph is None:
-                    self.reconnect()
-    
+            self.write("A")
+            if self._periph.waitForNotifications(1):
+                print("waiting, ...", self.beetle_id)
+                continue
 
     
 
 if __name__ == "__main__":
-    beetle_1 = Beetle_Connection(mac_address_1)
-    beetle_1.main_routine()
-    print(buffer)
-
-
-    # To check if bluetooth is working
-    # test = btle.Peripheral(mac_address_1)
-    # print("connected")
-    # test.disconnect()
-    # print("disconnected")
+    beetle_1 = Beetle_Connection(mac_address_1, 0)
+    thread_1 = threading.Thread(target=beetle_1.main_routine, args=())
+    beetle_2 = Beetle_Connection(mac_address_2, 1)
+    thread_2 = threading.Thread(target=beetle_2.main_routine, args=())
+    
+    thread_1.start()
+    thread_2.start()
